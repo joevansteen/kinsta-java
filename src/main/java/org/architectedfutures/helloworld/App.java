3d6e22ce887564c40c84588372526d35061cfbf3
joevansteen <joe.vansteen@gmail.com>
2023-05-29 11:32:32 -0700

    Begin expansion of code

--- src/main/java/org/architectedfutures/helloworld/App.java
+++ src/main/java/org/architectedfutures/helloworld/App.java
@@ -1,10 +1,12 @@
 package org.architectedfutures.helloworld;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Optional;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -24,10 +26,25 @@ public class App {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World from EATSv5";
+            String response = "Hello World from EATSv5 ";
+            
+            // Determine the request method
+            String method = t.getRequestMethod();
+            
+            // Examine the request headers
+            Headers hdrs = t.getRequestHeaders();
+            
+            // Examine the request body
+            InputStream is = t.getRequestBody();
+            
+            // Send response headers
             t.sendResponseHeaders(200, response.length());
+            // Get response output stream
             OutputStream os = t.getResponseBody();
+            // Send response content
             os.write(response.getBytes());
+            os.write(method.getBytes());
+            // Terminate exchange
             os.close();
         }
     }
